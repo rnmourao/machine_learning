77156e0f524429b5a3be26b19a8fa6a3db01f59f
rnmourao <robertonunesmourao@yahoo.com.br>
2016-08-01 11:45:24 -0300

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -1,5 +1,3 @@
-# this algorithm is not completed.
-
 import itertools
 import collections
 
@@ -8,16 +6,43 @@ def apriori(data, support_ratio):
     # get number of itemsets
     total_itemsets = len(data)
 
-    # get individual items
-    elements = frozenset([item for itemset in data for item in itemset])
+    # control size of subsets
+    length = 0
+    longest_itemset = max([len(itemset) for itemset in data])
 
-    # generate combinations of items
-    items = [x for i in range(len(elements) + 1) for x in itertools.combinations(elements, i) if x]
+    # initialize control of accepted subsets
+    support = {}
 
-    # count combined items in data
-    s = collections.Counter([i for i in items for itemset in data if itemset.issuperset(i)])
+    # initialize control of rejected subsets
+    discards = []
 
-    # remove itemsets that don't have minimum support
-    support = {key: value / total_itemsets for key, value in s.items() if value / total_itemsets >= support_ratio}
+    # stopping criteria - if a iteration is rejected completely, it's time to stop
+    foundit = True
+
+    while foundit and length <= longest_itemset:
+        # increment size of subsets
+        length += 1
+
+        # generate subsets and remove not supported combinations
+        items = set([combination
+                     for itemset in data
+                     for combination in itertools.combinations(itemset, length)
+                     if combination not in discards])
+
+        # count subsets in data
+        s = collections.Counter([item
+                                 for item in items
+                                 for itemset in data
+                                 if itemset.issuperset(item)])
+
+        # add subsets that have support and discard those don't have
+        foundit = False
+        for key, value in s.items():
+            ratio = value / total_itemsets
+            if ratio >= support_ratio:
+                foundit = True
+                support[key] = ratio
+            else:
+                discards += key
 
     return support
