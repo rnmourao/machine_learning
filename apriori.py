2b51fcec1b9f7323244046a1389944ec373ff840
rnmourao <robertonunesmourao@yahoo.com.br>
2016-08-01 17:32:07 -0300

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -3,6 +3,22 @@ import collections
 
 
 def apriori(data, support_ratio):
+    """
+    Given a list of sets (data), discover subsets that occurs at a minimum percentage (support_ratio).
+    Example:
+        data: [{1, 2, 3, 4}, {1, 2, 3}, {1, 2}, {2, 3, 4}, {2, 3}, {3, 4}, {2, 4}]
+        support_ratio: 0.42
+        Returns:
+            {(1,): 0.42857142857142855,
+             (1, 2): 0.42857142857142855,
+             (2,): 0.8571428571428571,
+             (2, 3): 0.5714285714285714,
+             (2, 4): 0.42857142857142855,
+             (3,): 0.7142857142857143,
+             (3, 4): 0.42857142857142855,
+             (4,): 0.5714285714285714}
+    """
+    
     # get number of itemsets
     total_itemsets = len(data)
 
